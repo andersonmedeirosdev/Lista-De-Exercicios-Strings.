630ccfc6f7dddac8d1b545d65b90f5c40da30dc3
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-29 17:19:28 -0300

    Finalizando ex005.

---
+++ impressaoPalavras/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml;
+
+namespace impressaoPalavras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "";
+
+            Console.WriteLine("Digite uma frase para manipularmos: ");
+            frase = Console.ReadLine();
+
+            Console.WriteLine("Em Maíusculas: " + frase.ToUpper());
+            Console.WriteLine();
+
+            Console.WriteLine("Em Mínusculas: " + frase.ToLower());
+            Console.WriteLine();
+
+            int qtdPalavras = frase.Split(" ").Count();
+            Console.WriteLine("A quantidade de Palavras é: " + qtdPalavras);
+            Console.WriteLine();
+
+            Console.WriteLine("A ultima palavra da frase é: " + frase.Split(' ').Last());
+        }
+    }
+}
