156de006118e73f5940c206ff93fe532e8c8a3c0
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-29 14:15:37 -0300

    Add project files.

---
+++ contarStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace contarStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma frase para contarmos o número de palavras: ");
+            string textoRecebido = Console.ReadLine();
+
+            int qtdPalavras = textoRecebido.Split(" ").Count();
+
+            Console.WriteLine("A quantidade de palavras é: " + qtdPalavras);
+            Console.ReadLine();
+        }
+    }
+}
